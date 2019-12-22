d9b1e37adc79315fb496d2182bc7e08d8c11e779
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2019-12-22 23:23:07 +0200

    added game module "brain-progression"

---
+++ src/games/games_brain-progression.js
@@ -0,0 +1,94 @@
+const gameInstruction = 'What number is missing in the progression?';
+
+const arithmeticProgression = (num1, num2) => {
+  let a = num1;
+  let result = '';
+  for (let count = 1; count <= 10; count += 1) {
+    a += num2;
+    result = `${result} ${a}`;
+  }
+  return result;
+};
+
+const hiddenElementAction1 = (progression) => {
+  const takeElement = (Math.floor(Math.random() * 10) + 9);
+  let caunt = takeElement;
+  for (; progression[caunt] !== ' ';) {
+    caunt += 1;
+  }
+  return caunt;
+};
+
+const hiddenElementAction2 = (progression) => {
+  const caunt = hiddenElementAction1(progression);
+  let result2 = '';
+  for (let caunt1 = caunt + 1; progression[caunt1] !== ' '; caunt1 += 1) {
+    result2 += progression[caunt1];
+  }
+  return result2;
+};
+
+const hiddenElementAction3 = (progression) => {
+  const caunt = hiddenElementAction1(progression);
+  const result2 = hiddenElementAction2(progression);
+  const elementLength = result2.length;
+  let result3 = '';
+  let caunt2 = 0;
+  for (; caunt2 <= caunt - 1; caunt2 += 1) {
+    result3 += progression[caunt2];
+  }
+  const a = caunt2 + elementLength;
+  let result4 = '';
+  for (let caunt3 = a + 1; caunt3 < progression.length; caunt3 += 1) {
+    result4 += progression[caunt3];
+  }
+  return `${result3} ..${result4}`;
+};
+
+const functionTheTask = () => {
+  const number1 = (Math.floor(Math.random() * 10) + 1);
+  const number2 = (Math.floor(Math.random() * 10) + 1);
+  const progression = arithmeticProgression(number1, number2);
+  const hiddenElemen = hiddenElementAction3(progression);
+  return hiddenElemen;
+};
+
+const takeNumber1Action1 = (theTask) => {
+  let result1 = '';
+  for (let caunt = 0; theTask[caunt] !== '.'; caunt += 1) {
+    result1 = `${result1}${theTask[caunt]}`;
+  }
+  return result1;
+};
+
+const takeNumber1Action2 = (theTask) => {
+  const result1 = takeNumber1Action1(theTask);
+  let result2 = '';
+  for (let caunt = result1.length - 2; result1[caunt] !== ' '; caunt -= 1) {
+    result2 = `${result1[caunt]}${result2}`;
+  }
+  return Number(result2);
+};
+
+const takeNumber2 = (theTask) => {
+  const result1 = takeNumber1Action1(theTask);
+  let result2 = '';
+  for (let caunt = result1.length + 3; theTask[caunt] !== ' '; caunt += 1) {
+    result2 = `${result2}${theTask[caunt]}`;
+  }
+  return Number(result2);
+};
+
+const functionRightAnswer = (theTask) => {
+  const number1 = takeNumber1Action2(theTask);
+  const number2 = takeNumber2(theTask);
+  const arithmeticProgressionDifference = (number2 - number1) / 2;
+  const result = number1 + arithmeticProgressionDifference;
+  return result;
+};
+
+const verification = (rightAnswer, question) => (rightAnswer !== Number(question));
+
+export {
+  gameInstruction, functionTheTask, functionRightAnswer, verification,
+};
